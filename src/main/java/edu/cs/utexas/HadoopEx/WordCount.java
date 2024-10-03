38b2a56d232b57fbe52d9ccbca118da3247a4bd1
nathanjacob <37455968+nathan-jacob@users.noreply.github.com>
2024-10-03 17:18:08 -0500

    works on cloud

--- src/main/java/edu/cs/utexas/HadoopEx/WordCount.java
+++ src/main/java/edu/cs/utexas/HadoopEx/WordCount.java
@@ -1,21 +1,13 @@
 package edu.cs.utexas.HadoopEx;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
@@ -23,102 +15,58 @@ import org.apache.hadoop.util.ToolRunner;
 
 public class WordCount extends Configured implements Tool {
 
-	/**
-	 * 
-	 * @param args
-	 * @throws Exception
-	 */
+    public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new Configuration(), new WordCount(), args);
+        System.exit(res);
+    }
 
-	public static void main(String[] args) throws Exception {
-		int res = ToolRunner.run(new Configuration(), new WordCount(), args);
-		System.exit(res);
-	}
+    public int run(String args[]) {
+        try {
+            double lr = 0.001;
+            double m = 0;
+            double b = 0;
 
-	/**
-	 * 
-	 */
-	public int run(String args[]) {
-		try {
+            for (int i = 0; i < 101; i++) {
+                System.out.println("M at iteration " + i + ": " + m);
+                System.out.println("B at iteration " + i + ": " + b);
 
-			double lr = 0.001;
-			double m = 0;
-			double b = 0;
-			for(int i = 0; i < 100; i++){
-				System.out.println("M at iteration " + i + ": " + m);
-				System.out.println("B at iteration " + i + ": " + b);
+                Configuration conf = new Configuration();
+                conf.set("current.m", Double.toString(m));
+                conf.set("current.b", Double.toString(b));
+                conf.setInt("current.iteration", i);
 
-				BufferedWriter writer = new BufferedWriter(new FileWriter("params", false));
-				writer.write(Double.toString(m) + "\n");
-				writer.write(Double.toString(b) + "\n");
-				writer.write(Integer.toString(i) + "\n");
-				writer.close();
+                Job job = Job.getInstance(conf, "WordCount");
+                job.setJarByClass(WordCount.class);
+                job.setMapperClass(WordCountMapper.class);
+                job.setReducerClass(WordCountReducer.class);
+                job.setOutputKeyClass(Text.class);
+                job.setOutputValueClass(Wrapper.class);
 
-				Configuration conf = new Configuration();
-				Job job = new Job(conf, "WordCount");
-				job.setJarByClass(WordCount.class);
+                FileInputFormat.addInputPath(job, new Path(args[0]));
+                job.setInputFormatClass(TextInputFormat.class);
+                FileOutputFormat.setOutputPath(job, new Path(args[1] + i));
+                job.setOutputFormatClass(TextOutputFormat.class);
 
-				// specify a Mapper
-				job.setMapperClass(WordCountMapper.class);
+                if (!job.waitForCompletion(true)) {
+                    return 1;
+                }
 
-				// specify a Reducer
-				job.setReducerClass(WordCountReducer.class);
+                // Read results from the job's counters
+                double mpart = job.getCounters().findCounter("GradientDescent", "mpart").getValue() / 1000000.0;
+                double bpart = job.getCounters().findCounter("GradientDescent", "bpart").getValue() / 1000000.0;
 
-				// specify output types
-				job.setOutputKeyClass(Text.class);
-				job.setOutputValueClass(Wrapper.class);
+                m -= mpart * lr;
+                b -= bpart * lr;
+            }
 
-				// specify input and output directories
-				FileInputFormat.addInputPath(job, new Path(args[0]));
-				job.setInputFormatClass(TextInputFormat.class);
+            System.out.println("Final M: " + m);
+            System.out.println("Final B: " + b);
 
-				FileOutputFormat.setOutputPath(job, new Path(args[1] + i));
-				job.setOutputFormatClass(TextOutputFormat.class);
-				job.waitForCompletion(true);
-
-				BufferedReader reader = new BufferedReader(new FileReader(args[1] + i + "/part-r-00000"));
-				
-				double mpart = Double.parseDouble(reader.readLine().split("\\s+")[1]);
-				double bpart = Double.parseDouble(reader.readLine().split("\\s+")[1]);
-
-				m -= mpart * lr;
-				b -= bpart * lr;
-			}
-
-			System.out.println("Final M: " + m);
-			System.out.println("Final B: " + b);
-
-			// if (!job.waitForCompletion(true)) {
-			// 	return 1;
-			// }
-
-			// Job job2 = new Job(conf, "TopK");
-			// job2.setJarByClass(WordCount.class);
-
-			// // specify a Mapper
-			// job2.setMapperClass(TopKMapper.class);
-
-			// // specify a Reducer
-			// job2.setReducerClass(TopKReducer.class);
-
-			// // specify output types
-			// job2.setOutputKeyClass(Text.class);
-			// job2.setOutputValueClass(FloatWritable.class);
-
-			// // set the number of reducer to 1
-			// job2.setNumReduceTasks(1);
-
-			// // specify input and output directories
-			// FileInputFormat.addInputPath(job2, new Path(args[1]));
-			// job2.setInputFormatClass(KeyValueTextInputFormat.class);
-
-			// FileOutputFormat.setOutputPath(job2, new Path(args[2]));
-			// job2.setOutputFormatClass(TextOutputFormat.class);
-			return 0;
-
-		} catch (InterruptedException | ClassNotFoundException | IOException e) {
-			System.err.println("Error during mapreduce job.");
-			e.printStackTrace();
-			return 2;
-		}
-	}
+            return 0;
+        } catch (InterruptedException | ClassNotFoundException | IOException e) {
+            System.err.println("Error during mapreduce job.");
+            e.printStackTrace();
+            return 2;
+        }
+    }
 }
